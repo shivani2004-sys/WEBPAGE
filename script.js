67c5ea39896f4a4a2ae71eaeedb290299c17c152
shivani2004-sys <shivaniroy4858@gmail.com>
2025-05-03 17:16:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMore() {
+  const moreText = document.getElementById("moreText");
+  if (moreText.style.display === "none") {
+    moreText.style.display = "block";
+  } else {
+    moreText.style.display = "none";
+  }
+}
